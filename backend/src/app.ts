39e9cd861414b7d9343b6a37982a6db809ac7e1a
Markus <markus.kitunen@outlook.com>
2024-10-19 20:57:04 +0300

    routes added to serve static files

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import config from './utils/config'
 import express, { Express, Request, Response } from 'express'
 import mongoose from 'mongoose'
 import announcementRouter from './controllers/announcement';
+import path from 'path';
 
 const app: Express = express();
 
@@ -14,10 +15,30 @@ mongoose
     console.error("error connection to MongoDB", error.message);
   });
 
-app.use(express.static("build"));
+
 app.use(express.json());
 
+app.use('/', (req, res, next) => {
+  if (req.url.startsWith('/announcements')) {
+    app.use(express.static(path.join(__dirname, '../build/announcementBoard')))
+  }
+  if (req.url.startsWith('/new')) {
+    app.use(express.static(path.join(__dirname, '../build/new')))
+  }
+  if (req.url.startsWith('/controller')) {
+    app.use(express.static(path.join(__dirname, '../build/controller')))
+  }
+  next();
+})
 
+
+//Routes
+app.use('/announcements', express.static('build/announcementBoard'))
+app.use('/new', express.static('build/new'));
+app.use('/controller', express.static('build/controller'));
+
+
+//APIs
 app.use('/api/announcement', announcementRouter)
 
 export default app
