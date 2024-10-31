046838f9c3cc6ddd244e1a33edc1599f866802b8
Markus <markus.kitunen@outlook.com>
2024-10-31 18:36:45 +0200

    authorzation middleware implemented

--- backend/src/utils/middleware.ts
+++ backend/src/utils/middleware.ts
@@ -1,4 +1,10 @@
+import config from './config'
 import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+
+export interface CustomRequest extends Request {
+  user: string | jwt.JwtPayload
+}
 
 export const checkTrailingSlash = (
   req: Request,
@@ -10,4 +16,27 @@ export const checkTrailingSlash = (
     return
   }
   next()
+}
+
+export const authorization = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token: string = req.cookies.acces_token
+
+  if (!token) {
+    res.status(401).json({
+      error: 'token missing'
+    })
+    return
+  }
+  try {
+    const decoded = jwt.verify(token, config.SECRET!)
+    ;(req as CustomRequest).user = decoded
+    next()
+  } catch (error) {
+    res.status(401).json({ error: 'invalid token' })
+    return
+  }
 }
