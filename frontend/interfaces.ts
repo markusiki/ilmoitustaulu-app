6151cdef8f145d6c6ac1e9bf3ee6aa69e048ec01
Markus <markus.kitunen@outlook.com>
2024-11-07 18:11:39 +0200

    added

---
+++ frontend/interfaces.ts
@@ -0,0 +1,38 @@
+export interface IAnnouncement {
+  id: string;
+  category: "asiakastoive" | "myynti-ilmoitus";
+  poster: string;
+  contact_info?: string;
+  title: string;
+  content: string;
+  file?: {
+    data: BinaryData;
+    type: string;
+  };
+}
+
+export interface IAdvertisement {
+  id: string;
+  file: {
+    data: BinaryData;
+    type: string;
+  };
+}
+
+export interface DataFromServer {
+  type:
+    | "initialdata"
+    | "advertisementadd"
+    | "advertisementdelete"
+    | "announcementadd"
+    | "announcementdelete"
+    | "newannouncementid";
+  data: {
+    announcements: IAnnouncement[];
+    advertisements: IAdvertisement[];
+    announcement: IAnnouncement | IAnnouncement[];
+    advertisement: IAdvertisement;
+    newAnnouncmentId: string;
+    id: string;
+  };
+}
